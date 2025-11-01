29990b5a58f779f3e9957290178798516f99b179
HELAFUG <loxand42@gmail.com>
2025-11-01 12:12:18 +0300

    add bearer dependencies in user router

--- app/api/api_v1/users.py
+++ app/api/api_v1/users.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+)
+from fastapi.security import HTTPBearer
 from core.config import settings
 from core.schemas.user import UserRead, UserUpdate
 from .fastapi_users import fastapi_users
 
-router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
+bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(
+    prefix=settings.api.v1.users, tags=["Users"], dependencies=[Depends(bearer)]
+)
 
 router.include_router(
     fastapi_users.get_users_router(
