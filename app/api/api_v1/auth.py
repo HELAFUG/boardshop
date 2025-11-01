c710ca38e076d3440be7681f647b09201cd997b9
HELAFUG <loxand42@gmail.com>
2025-11-01 11:39:17 +0300

    solve connection problem

--- app/api/api_v1/auth.py
+++ app/api/api_v1/auth.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from core.config import settings
+from api.dependencies.authentication import auth_backend
 from .fastapi_users import fastapi_users
 
 
-router = APIRouter(prefix=settings.api.v1.auth)
-router.include_router(fastapi_users.get_auth_router())
+router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
+router.include_router(fastapi_users.get_auth_router(backend=auth_backend))
--- app/api/dependencies/authentication/backend.py
+++ app/api/dependencies/authentication/backend.py
@@ -1,7 +1,7 @@
 from fastapi_users.authentication import AuthenticationBackend
 from core.authentication.transport import bearer_transport
+from .strategy import get_db_strategy
 
 auth_backend = AuthenticationBackend(
-    name="access-token",
-    transport=bearer_transport,
+    name="access-token", transport=bearer_transport, get_strategy=get_db_strategy
 )
