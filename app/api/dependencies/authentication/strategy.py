d28949a489abdf869bd9f8d5cee2d51a8b1a9af7
HELAFUG <loxand42@gmail.com>
2025-11-01 10:48:58 +0300

    add database strategy

--- app/api/dependencies/authentication/strategy.py
+++ app/api/dependencies/authentication/strategy.py
@@ -1,13 +1,6 @@
-from typing import (
-    TYPE_CHECKING,
-    Annotated,
-)
-
+from typing import TYPE_CHECKING, Annotated
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import (
-    DatabaseStrategy,
-)
-
 from core.config import settings
 from .access_tokens import get_access_tokens_db
 
@@ -16,13 +9,12 @@ if TYPE_CHECKING:
     from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 
-def get_database_strategy(
-    access_tokens_db: Annotated[
-        "AccessTokenDatabase[AccessToken]",
-        Depends(get_access_tokens_db),
+async def get_db_strategy(
+    access_token_db: Annotated[
+        "AccessTokenDatabase[AccessToken]", Depends(get_access_tokens_db)
     ],
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
-        database=access_tokens_db,
+        database=access_token_db,
         lifetime_seconds=settings.access_token.lifetime_seconds,
     )
