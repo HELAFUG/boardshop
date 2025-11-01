0087d0b6222956dd7e3e845663718726cdcc881e
HELAFUG <loxand42@gmail.com>
2025-11-01 10:53:21 +0300

    forgot add user manager to init

--- app/api/dependencies/authentication/__init__.py
+++ app/api/dependencies/authentication/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "get_access_tokens_db",
     "get_users_db",
     "get_db_strategy",
+    "get_user_manager",
 )
 
 from .backend import auth_backend
 from .access_tokens import get_access_tokens_db
 from .users import get_users_db
 from .strategy import get_db_strategy
+from .user_manager import get_user_manager
