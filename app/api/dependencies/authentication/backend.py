2ad6d0a272c11eab4d0eb399616fe08346538ef3
HELAFUG <loxand42@gmail.com>
2025-11-01 10:51:51 +0300

    some reload in backend dev

--- app/api/dependencies/authentication/backend.py
+++ app/api/dependencies/authentication/backend.py
@@ -1,1 +1,7 @@
-from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
+from fastapi_users.authentication import AuthenticationBackend
+from core.authentication.transport import bearer_transport
+
+auth_backend = AuthenticationBackend(
+    name="access-token",
+    transport=bearer_transport,
+)
